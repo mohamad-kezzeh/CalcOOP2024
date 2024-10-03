c8ccb0f90a562eb3e021d6390d27258f926dbbcd
Mohamad Kezzeh <g00423815@atu.ie>
2024-10-03 21:54:28 +0100

    added new method called add

--- src/main/java/ie/atu/week3/Calc.java
+++ src/main/java/ie/atu/week3/Calc.java
@@ -1,7 +1,25 @@
 package ie.atu.week3;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
 
+        System.out.println("Please Enter a number: ");
+        Scanner scan1 = new Scanner(System.in);
+        int a = scan1.nextInt();
+        System.out.println("please enter a second number: ");
+        int b = scan1.nextInt();
+        System.out.print("Choose an operation ");
+        String operation = scan1.next();
+        switch (operation){
+            case "add": add(a, b); break;
+            default: System.out.println("Invalid operation"); break;
+        }
+
+    }
+    public static void add(int a, int b){
+        int sum = a + b;
+        System.out.println(sum);
     }
 }
